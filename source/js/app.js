3d1392bddcc1532c3ecd978fd25dc83b2092e64d
Nikita <nekmalienko2001@gmail.com>
2021-01-09 22:42:23 +0200

    product desktop 90%

--- source/js/app.js
+++ source/js/app.js
@@ -15,3 +15,13 @@
        nav.classList.remove('show');
     });
 }
+
+const price = document.querySelector('.price');
+const check = document.querySelector('.check');
+
+check.addEventListener('click', ()=>{
+    const current = check.querySelector('[name="size"]:checked');
+    if(current){
+        price.innerText =  current.value
+    }
+});
---
+++ source/js/inner.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+
+    if (window.matchMedia('(max-width: 991px)').matches) {
+        // do functionality on screens smaller than 768px
+        $('#productSlider').lightSlider({
+            gallery: true,
+            item: 1,
+            vertical: false,
+            // adaptiveHeight: true,
+            // verticalHeight: 196,
+            slideMargin: 5
+        });
+    }
+    if (window.matchMedia('(min-width: 992px)').matches) {
+        $('#productSlider').lightSlider({
+            gallery: true,
+            item: 1,
+            vertical: true,
+            adaptiveHeight: true,
+            loop: true,
+            verticalHeight: 440,
+            vThumbWidth: 60,
+            thumbItem: 5,
+            thumbMargin: 4,
+            slideMargin: 0
+        });
+    }
+});
